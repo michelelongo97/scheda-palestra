e6c5d8d7dca6f2e35805bd4717297ab0adb96e21
michelelongo97 <m.longo7x@gmail.com>
2025-12-12 17:54:19 +0100

    feat: add sponsori imgs and links

--- scheda-palestra-fe/src/components/Footer.jsx
+++ scheda-palestra-fe/src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
         <div className="row">
           {/* COLONNA SOCIAL */}
           <div className="col footer-social">
-            <h4>Seguimi sui social</h4>
+            <h4>Seguimi</h4>
             <ul>
               <li>
                 <a
@@ -59,22 +59,59 @@ export default function Footer() {
           </div>
 
           {/* COLONNA SPONSOR */}
-          <div className="col">
-            <h4>Sponsor</h4>
+          <div className="col footer-sponsor">
+            <h4>I Nostri Partner</h4>
             <ul>
-              <li>Sponsor 1</li>
-              <li>Sponsor 2</li>
-              <li>Sponsor 3</li>
-              <li>Sponsor 4</li>
+              <li>
+                <a
+                  href={import.meta.env.VITE_SPONSOR_MATERA_URL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="./images/matera-logo.png" alt="matera-logo" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href={import.meta.env.VITE_SPONSOR_GIANNELLI_URL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="./images/giannelli-logo.png" alt="giannelli-logo" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href={import.meta.env.VITE_SPONSOR_GALLERY_URL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="./images/gallery-logo.png" alt="gallery-logo" />
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href={import.meta.env.VITE_SPONSOR_INTERNO34_URL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="./images/interno34-logo.png" alt="interno34-logo" />
+                </a>
+              </li>
             </ul>
           </div>
           {/* COLONNA INFO LEGALI */}
           <div className="col footer-info">
             <h4>Informazioni</h4>
-            <p>FisioFitness Dr. Costantino Picciallo</p>
-            <p>P.IVA 00000000000</p>
-            <p>Email: email@email.it</p>
-            <p>Sede: Gravina in Puglia (BA), Italia</p>
+            <p>
+              FisioFitness<br></br>Dr. Costantino Picciallo
+            </p>
+            <p>P.IVA: 08494380721</p>
+            <p>email</p>
+            <p>
+              Via Palermo, 26<br></br> Gravina in Puglia (BA)
+            </p>
           </div>
         </div>
         {/* COPYRIGHT */}
